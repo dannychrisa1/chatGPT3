0177adcb21c186d6f92fc14b879518dc4ae30d29
dannychrisa1 <dannychris.tk@gmail.com>
2023-03-16 10:38:10 +0100

    initial-commit

---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+export { default as Blog } from './blog/blog';
+export { default as Features } from './features/features';
+export { default as Footer } from './footer/footer';
+export { default as Header } from './header/header';
+export { default as Possibility } from './possibilty/possibility';
+export {default as WhatGPT3 } from './whatGPT3/whatGPT3';
+
